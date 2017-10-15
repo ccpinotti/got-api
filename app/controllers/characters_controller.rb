2b91a4918c6223419961e073d600cdc1d0aec92b
ccpinotti <ccpinotti@gmail.com>
2017-10-15 14:06:58 -0700

    added create and edit functionality

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,8 +1,14 @@
 class CharactersController < ApplicationController
   def index
+    @characters = Character.all
+  end
+
+  #renders submission
+  def new
   end
 
   def edit
+    @character = Character.find(params[:id])
   end
 
   def show
@@ -11,9 +17,15 @@ class CharactersController < ApplicationController
   def destroy
   end
 
+  #create and update database for new character
   def create
+    Character.create(name: params[:name], bio: params[:bio])
+    redirect_to "/characters"
   end
 
   def update
+    @character = Character.find(params[:id])
+    @character.update_attributes(name: params[:character][:name], bio: params[:character][:bio])
+    redirect_to '/characters'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#index'
-  
-  resource :characters
+
+  resources :characters
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
